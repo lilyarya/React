57c12f0ab15794bf8143f5fd98ff0820489868f7
lilyarya <91736494+lilyarya@users.noreply.github.com>
2023-06-17 16:46:03 +0530

    second commit in class comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+import React from "react";
 
 
-
-function App() {
-  return (
-   <h1>Functional Components
-
-   </h1>
-  );
+class App extends React.Component{
+  render(){
+    return <h1>Class Component</h1>
+  }
 }
 
 export default App;
